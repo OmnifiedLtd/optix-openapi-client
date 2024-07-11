15634728d96c1c251a658c242a34d4a3af7d7d88
Adam Harris <adam@omnified.io>
2024-07-11 18:15:30 +0200

    made successful request in integration test

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,13 +1,17 @@
 import { expect, test } from 'vitest'
 import { newClient } from '../client'
-import { searchProducts } from '../product'
+import { ProductSearchInput, ProductType, searchProducts } from '../product'
 import 'dotenv/config'
 
 test('1==1', async () => {
   const apiKey = process.env.OPTIX_API_KEY!
   const client = newClient(apiKey)
-  const input: any = {}
+  const input: ProductSearchInput = {
+    productType: ProductType.Sunglasses,
+    pageSize: 2
+  }
   const response = await searchProducts(client, input)
   console.log(response)
+  console.log(response.data)
   expect(1).toEqual(1)
 })
