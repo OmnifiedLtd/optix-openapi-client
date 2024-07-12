c72592ee63871056eeffd16a339ce1e93199f5c9
Adam Harris <adam@omnified.io>
2024-07-12 12:57:09 +0200

    defined a mockable client type

---
+++ api.ts
@@ -0,0 +1,45 @@
+import { productGetPrices, productGetPricesExn, ProductGetPricesInput, ProductGetPricesResponse, productGetRetailPrices, productGetRetailPricesExn, ProductGetRetailPricesInput, ProductGetRetailPricesResponse, productGetSkus, productGetSkusExn, ProductGetSkusInput, ProductGetSkusResponse, ProductSearchInput, ProductSearchResponse, productsSearch, productsSearchExn } from "./live/product"
+import { ApiResponse } from "./response"
+import { skuGetDetails, skuGetDetailsExn, SkuGetDetailsInput, SkuGetDetailsResponse, skuGetPrices, skuGetPricesExn, SkuGetPricesInput, SkuGetPricesResponse, skuGetStockLevels, skuGetStockLevelsExn, SkuGetStockLevelsInput, SkuGetStockLevelsResponse, skuUpdateStockLevelBySku, skuUpdateStockLevelBySkuExn, SkuUpdateStockLevelBySkuInput, SkuUpdateStockLevelBySkuResponse } from "./live/sku"
+import { newAuthenticatedClient } from "./live/client"
+
+export type OptixApiClient = {
+  productGetSkus: (input: ProductGetSkusInput) => Promise<ApiResponse<ProductGetSkusResponse>>
+  productGetSkusExn: (input: ProductGetSkusInput) => Promise<ProductGetSkusResponse>
+  productGetPrices: (input: ProductGetPricesInput) => Promise<ApiResponse<ProductGetPricesResponse>>
+  productGetPricesExn: (input: ProductGetPricesInput) => Promise<ProductGetPricesResponse>
+  productGetRetailPrices: (input: ProductGetRetailPricesInput) => Promise<ApiResponse<ProductGetRetailPricesResponse>>
+  productGetRetailPricesExn: (input: ProductGetRetailPricesInput) => Promise<ProductGetRetailPricesResponse>
+  productsSearch: (input: ProductSearchInput) => Promise<ApiResponse<ProductSearchResponse>>
+  productsSearchExn: (input: ProductSearchInput) => Promise<ProductSearchResponse>
+  skuGetStockLevels: (input: SkuGetStockLevelsInput) => Promise<ApiResponse<SkuGetStockLevelsResponse>>
+  skuGetStockLevelsExn: (input: SkuGetStockLevelsInput) => Promise<SkuGetStockLevelsResponse>
+  skuGetDetails: (input: SkuGetDetailsInput) => Promise<ApiResponse<SkuGetDetailsResponse>>
+  skuGetDetailsExn: (input: SkuGetDetailsInput) => Promise<SkuGetDetailsResponse>
+  skuGetPrices: (input: SkuGetPricesInput) => Promise<ApiResponse<SkuGetPricesResponse>>
+  skuGetPricesExn: (input: SkuGetPricesInput) => Promise<SkuGetPricesResponse>
+  skuUpdateStockLevelBySku: (input: SkuUpdateStockLevelBySkuInput) => Promise<ApiResponse<SkuUpdateStockLevelBySkuResponse>>
+  skuUpdateStockLevelBySkuExn: (input: SkuUpdateStockLevelBySkuInput) => Promise<SkuUpdateStockLevelBySkuResponse>
+}
+
+export function newLiveClient(apiKey: string): OptixApiClient {
+  const authenticatedClient = newAuthenticatedClient(apiKey)
+  return {
+    productGetSkus: (input) => productGetSkus(authenticatedClient, input),
+    productGetSkusExn: (input) => productGetSkusExn(authenticatedClient, input),
+    productGetPrices: (input) => productGetPrices(authenticatedClient, input),
+    productGetPricesExn: (input) => productGetPricesExn(authenticatedClient, input),
+    productGetRetailPrices: (input) => productGetRetailPrices(authenticatedClient, input),
+    productGetRetailPricesExn: (input) => productGetRetailPricesExn(authenticatedClient, input),
+    productsSearch: (input) => productsSearch(authenticatedClient, input),
+    productsSearchExn: (input) => productsSearchExn(authenticatedClient, input),
+    skuGetStockLevels: (input) => skuGetStockLevels(authenticatedClient, input),
+    skuGetStockLevelsExn: (input) => skuGetStockLevelsExn(authenticatedClient, input),
+    skuGetDetails: (input) => skuGetDetails(authenticatedClient, input),
+    skuGetDetailsExn: (input) => skuGetDetailsExn(authenticatedClient, input),
+    skuGetPrices: (input) => skuGetPrices(authenticatedClient, input),
+    skuGetPricesExn: (input) => skuGetPricesExn(authenticatedClient, input),
+    skuUpdateStockLevelBySku: (input) => skuUpdateStockLevelBySku(authenticatedClient, input),
+    skuUpdateStockLevelBySkuExn: (input) => skuUpdateStockLevelBySkuExn(authenticatedClient, input),
+  }
+}
--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,28 +1,28 @@
 import { expect, test } from 'vitest'
-import { newClient } from '../client'
-import { ProductSearchInput, ProductType, searchProducts } from '../product'
+import { newLiveClient } from '../api'
 import 'dotenv/config'
 import { ApiErrorResponse } from '../response'
+import { ProductType } from '../live/product'
 
 test('search products', async () => {
   const apiKey = process.env.OPTIX_API_KEY!
-  const client = newClient(apiKey)
-  const input: ProductSearchInput = {
+  const client = newLiveClient(apiKey)
+  const input = {
     productType: ProductType.Sunglasses,
     pageSize: 2
   }
-  const response = await searchProducts(client, input)
+  const response = await client.productsSearch(input)
   expect(response.type).toEqual('Success')
 })
 
 test('unauthorized error', async () => {
   const apiKey = "invalidApiKey"
-  const client = newClient(apiKey)
-  const input: ProductSearchInput = {
+  const client = newLiveClient(apiKey)
+  const input = {
     productType: ProductType.Sunglasses,
     pageSize: 2
   }
-  const response = await searchProducts(client, input)
+  const response = await client.productsSearch(input)
   expect(response.type).toEqual('Error')
   const { error } = response as ApiErrorResponse
   expect(error.type).toEqual('Unauthorized')
@@ -30,11 +30,11 @@ test('unauthorized error', async () => {
 
 test('bad request error', async () => {
   const apiKey = process.env.OPTIX_API_KEY!
-  const client = newClient(apiKey)
-  const badInput: ProductSearchInput = {
+  const client = newLiveClient(apiKey)
+  const badInput = {
     productTipe: ProductType.Sunglasses,
   } as any
-  const response = await searchProducts(client, badInput)
+  const response = await client.productsSearch(badInput)
   expect(response.type).toEqual('Error')
   const { error } = response as ApiErrorResponse
 
